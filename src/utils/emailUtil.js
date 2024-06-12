75a2e1ec44a47e4faea1ad92de3fd2fa283155f4
Alix Carmo <101589774+AlixAPCarmo@users.noreply.github.com>
2024-06-12 12:50:15 +0100

    update email util

--- src/utils/emailUtil.js
+++ src/utils/emailUtil.js
@@ -1,8 +1,8 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  host: 'smtp.gmail.com',
+  port: 465,
   secure: true, 
   auth: {
     user: process.env.EMAIL_USERNAME,
@@ -11,8 +11,8 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendVerificationEmail = async (to, token) => {
-  const subject = "Email Verification";
-  const verificationUrl = `${'https://86ff-2001-8a0-dd94-bf01-da3a-ddff-fee0-61fa.ngrok-free.app/api'}/verify-email/${token}`;
+  const subject = 'Email Verification';
+  const verificationUrl = `https://86ff-2001-8a0-dd94-bf01-da3a-ddff-fee0-61fa.ngrok-free.app/api/verify-email/${token}`;
   const html = `
     <h1>Email Verification</h1>
     <p>Please verify your email by clicking on the link below:</p>
@@ -27,14 +27,14 @@ const sendVerificationEmail = async (to, token) => {
       html,
     });
 
-    console.log("Verification email sent successfully");
+    console.log('Verification email sent successfully');
   } catch (error) {
-    console.error("Error sending verification email:", error);
+    console.error('Error sending verification email:', error);
   }
 };
 
 const sendPasswordResetEmail = async (to, token) => {
-  const subject = "Password Reset";
+  const subject = 'Password Reset';
   const html = `
     <h1>Password Reset</h1>
     <p>To reset your password, use the following token:</p>
@@ -50,12 +50,10 @@ const sendPasswordResetEmail = async (to, token) => {
       html,
     });
 
-    console.log("Password reset email sent successfully");
+    console.log('Password reset email sent successfully');
   } catch (error) {
-    console.error("Error sending password reset email:", error);
+    console.error('Error sending password reset email:', error);
   }
-}
+};
 
-
-export { sendVerificationEmail };
-export { sendPasswordResetEmail };
+export { sendVerificationEmail, sendPasswordResetEmail };
